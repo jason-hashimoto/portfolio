5c0c9cf05020f991c79d5032ee73a50c0d342998
jason-hashimoto <hashimotojason78@gmail.com>
2024-10-02 22:24:41 -0700

    resume + life updates

--- assets/js/typeIt.js
+++ assets/js/typeIt.js
@@ -7,10 +7,10 @@ new TypeIt("#typed", {
 })
 .type("<span class='user'>jason@portfolio</span>:<span class='asterisk'>~</span>$ cd /profile/jason-hashimoto<br>",{ delay: 500 })
 .type("<span class='user'>jason@portfolio</span>:<span class='asterisk'>/profile/jason-hashimoto</span>$ ls<br>",{ delay: 500 })
-.type("job: sports science I, driveline baseball<br>")
+.type("job: applied biomechanist, chicago white sox<br>")
 .type("interests: sports, cooking, climbing<br>")
 .type("eduction: b.s. kinesiology, oregon state univeristy, m.s. biomechanics, point loma nazarene university<br>")
-.type("upcoming-events: isbs 2023<br>")
+.type("upcoming-events: trip to japan<br>")
 .type("<span class='user'>jason@portfolio</span>:<span class='asterisk'>/profile/jason-hashimoto</span>$ ")
 .go();
 
